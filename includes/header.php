36c78d330672a59943a2bc03f8b6571215b218c2
Maurice <molande.mau@gmail.com>
2025-09-05 19:47:40 +0200

    changing nav algo 3

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Bootstrap 5 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="./assets/css/style.css" rel="stylesheet">
+  <link href="/assets/css/style.css" rel="stylesheet">
 </head>
 <body class="d-flex flex-column min-vh-100">
 
