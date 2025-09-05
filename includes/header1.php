449237de8940056c9d8334ce708d2c8aa422a558
Maurice <molande.mau@gmail.com>
2025-09-05 19:41:26 +0200

    changing nav algo in header 2

--- includes/header1.php
+++ includes/header1.php
@@ -13,14 +13,14 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="/akuua/index.php">🛍 Akuua</a>
+    <a class="navbar-brand" href="index.php">🛍 Akuua</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link" href="/akuua/public/cart.php">Cart</a></li>
-        <li class="nav-item"><a class="nav-link" href="/akuua/public/order_status.php">Track Order</a></li>
+        <li class="nav-item"><a class="nav-link" href="public/cart.php">Cart</a></li>
+        <li class="nav-item"><a class="nav-link" href="public/order_status.php">Track Order</a></li>
       </ul>
     </div>
   </div>
