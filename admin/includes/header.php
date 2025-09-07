f00bec5d5497576019eb0ec8df33b8ade5deb5db
Maurice <molande.mau@gmail.com>
2025-09-07 09:03:13 +0200

    admin header fx 2

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -16,7 +16,7 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="/admin/dashboard.php">🛍 Akuua</a>
+    <a class="navbar-brand" href="../admin/dashboard.php">🛍 Akuua</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
