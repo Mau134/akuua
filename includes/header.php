dadab3d56ca29b95b103c94d97bef4b90bb202cb
Maurice <molande.mau@gmail.com>
2025-09-12 06:55:25 +0200

    nav bar fix

--- includes/header.php
+++ includes/header.php
@@ -7,24 +7,58 @@
   <!-- Bootstrap 5 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="/assets/css/stylec.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+
 </head>
 <body class="d-flex flex-column min-vh-100">
 
-<!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="index.php">🛍 Akuua</a>
+    <!-- Brand Logo -->
+    <a class="navbar-brand" href="../index.php">
+      <i class="fas fa-shopping-bag me-2"></i> Akuua
+    </a>
+
+    <!-- Toggler -->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
+
+    <!-- Navbar Links -->
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link" href="../public/cart.php">Cart</a></li>
-        <li class="nav-item"><a class="nav-link" href="../public/order_status.php">Track Order</a></li>
+        <!-- About Us -->
+        <li class="nav-item">
+          <a class="nav-link" href="/index.php#about">
+            <i class="fas fa-info-circle me-1"></i> About Us
+          </a>
+        </li>
+
+        <!-- FAQ -->
+        <li class="nav-item">
+          <a class="nav-link" href="/index.php#faq">
+            <i class="fas fa-question-circle me-1"></i> FAQ
+          </a>
+        </li>
+
+        <!-- Cart -->
+        <li class="nav-item">
+          <a class="nav-link" href="../public/cart.php">
+            <i class="fas fa-shopping-cart me-1"></i> Cart
+          </a>
+        </li>
+
+        <!-- Track Order -->
+        <li class="nav-item">
+          <a class="nav-link" href="../public/order_status.php">
+            <i class="fas fa-truck me-1"></i> Track Order
+          </a>
+        </li>
       </ul>
     </div>
   </div>
 </nav>
+
 
 <!-- Main content wrapper -->
 <main class="flex-grow-1">
