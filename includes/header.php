6ed687407f6ca8fa77809e31c1144643e184f740
Maurice <molande.mau@gmail.com>
2025-09-20 07:55:11 +0200

    commenting out log in header

--- includes/header.php
+++ includes/header.php
@@ -73,7 +73,8 @@ $username = $is_logged_in ? htmlspecialchars($_SESSION['username']) : null;
         </li>
 
         <!-- Login / Logout -->
-        <?php if (!$is_logged_in): ?>
+        <?php /*
+        if (!$is_logged_in): ?>
           <li class="nav-item">
             <a class="nav-link" href="../public/login.php">
               <i class="fas fa-user me-1"></i> Log In
@@ -88,7 +89,9 @@ $username = $is_logged_in ? htmlspecialchars($_SESSION['username']) : null;
               <li><a class="dropdown-item" href="../public/logout.php">Logout</a></li>
             </ul>
           </li>
-        <?php endif; ?>
+        <?php endif;
+        */
+        ?>
 
       </ul>
     </div>
