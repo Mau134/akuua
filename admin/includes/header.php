e656fdc7a0c551f00c6001369af4dd5f2bed6fb1
Maurice <molande.mau@gmail.com>
2025-09-24 16:54:41 +0200

    favicon

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -9,7 +9,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/css/style.css" rel="stylesheet">
   <link href="../assets/css/style.css" rel="stylesheet">
-  <link rel="icon" type="image/png" href="../assets/images/Logo.png">
+  <link rel="icon" type="image/png" href="../assets/images/Logo.jpg">
 
 
 </head>
