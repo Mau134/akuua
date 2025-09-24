55479a5f53eed9c9309c17aca3f1bb7e33093509
Maurice <molande.mau@gmail.com>
2025-09-24 16:50:26 +0200

    adding a favicon for the site

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -9,6 +9,8 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/css/style.css" rel="stylesheet">
   <link href="../assets/css/style.css" rel="stylesheet">
+  <link rel="icon" type="image/png" href="../assets/images/Logo.png">
+
 
 </head>
 <body class="d-flex flex-column min-vh-100">
