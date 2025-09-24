bb872a01439246d7d1decefe2cb69f8c6aaa3a8d
Maurice <molande.mau@gmail.com>
2025-09-24 16:51:06 +0200

    adding fav icn

--- includes/header1.php
+++ includes/header1.php
@@ -18,6 +18,8 @@ $username = $is_logged_in ? htmlspecialchars($_SESSION['username']) : null;
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="../assets/css/style.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+  <link rel="icon" type="image/png" href="../assets/images/Logo.png">
+
 </head>
 <body class="d-flex flex-column min-vh-100">
 
